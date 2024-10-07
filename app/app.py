489ca7fac382aa228b790e67338ec917304e667f
Azathoth-X <ranaprikshit25@gmail.com>
2024-10-07 14:52:09 +0530

    clean code app.py

--- app/app.py
+++ app/app.py
@@ -60,24 +60,10 @@ async def websocket_endpoint(websocket: WebSocket):
             data = await websocket.receive_text()
             message = json.loads(data)
 
-            # If message contains the future time for data collection
-            # if message['type'] == 'SYNC_START_TIME':
-            #     future_epoch_time = message['start_time']
-            #     print(f"Received future start time: {future_epoch_time}")
-                
-            #     # Set the global start time
-            #     global_start_time = future_epoch_time
-
-            #     # Send acknowledgment back to the server
-            #     await websocket.send_text(json.dumps({'type': 'TIME_SYNC_ACK', 'status': 'completed'}))
-
-            #     # Start a background thread to wait until the start time and start collection
-            #     threading.Thread(target=collect_data(), daemon=True).start()
-
             if message['type'] == 'START_ANALYSIS':
                 # Directly start the collection if requested
                 # result = collect_data()
-                result=threading.Thread(target=collect_data,args=("test","kewk"), daemon=True).start()
+                result=threading.Thread(target=collect_data,args=("test","test"), daemon=False).start()
                 await websocket.send_text(json.dumps({'type': 'ANALYSIS_RESULT', 'result': 'starting'}))
                 result= result_queue.get()
                 # if result:
@@ -85,12 +71,6 @@ async def websocket_endpoint(websocket: WebSocket):
                 # await websocket.close()
                 # print('socket closed ')
                 break
-
-
-            
-            
-
-
 
     except WebSocketDisconnect:
         print("WebSocket connection closed")
